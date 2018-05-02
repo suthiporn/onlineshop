6896b0d0c9bacca154cc4642b73f138fb7e411b4
zesani <7sin@outlook.co.th>
2018-05-02 20:28:34 +0700

    interface only

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import VueFire from 'vuefire'
+Vue.use(VueFire)
+
+require('./assets/bootstrap/css/bootstrap.css')
+require('./assets/bootstrap/css/bootstrap.min.css')
+require('./assets/bootstrap/css/bootstrap-responsive.css')
+require('./assets/bootstrap/css/bootstrap-responsive.min.css')
+require('./assets/themes/css/bootstrappage.css')
+//require('./assets/themes/css/flexslider.css')
+//require('./assets/themes/css/jquery.fancybox.css')
+//require('./assets/themes/css/main.css')
+
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
