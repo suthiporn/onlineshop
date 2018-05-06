2adf58f33a0c9cd6e225d4c61bc43768514574ac
Chayanut Pasing <wthnfbest@gmail.com>
2018-05-06 19:51:47 +0700

    check the fill

--- src/App.vue
+++ src/App.vue
@@ -13,67 +13,45 @@
 			<img class="pageBanner" src="https://cdn.shopify.com/s/files/1/0214/5470/files/OLN-SLIDER-1.png?8247432977261988465"  >
 			</div>
       <div class="" style="margin-top:20px">
-        <h2>Welcome</h2> <br>  
+        <h1>Welcome</h1> <br>
 			<img class="imgEdit" src="https://scontent.fbkk21-1.fna.fbcdn.net/v/t1.0-9/27459244_2155848374701581_4375726229725598849_n.jpg?_nc_cat=0&oh=4517b2c5d3326fce2338f435696b7ac4&oe=5B8802CE"  >
-			</div>
+      <hr>
+      </div>
 				<div class="row" style="margin-top:20px">
 					<div class="col-12" >
-						<ul class="">
+						<ul >
 							<li class="col-3" v-for="product in productFire" :key="product['.key']">
-								<div class="product-box">
-									<span class="sale_tag"></span>
-									<a href="/"><img :src="product.imgLink"></a><br/>
-									{{product.productName}}<br/>
-									<p class="price"><h4>{{product.price}} ฿</h4> </p>
-                  {{product.amount}} left <br>
-                  <a class="button is-link is-outlined">Add to cart</a>
-								</div>
+									<a href="/"><img :src="product.imgLink" width="300" height="255"></a><br/>
+                    {{product.productName}}<br/>
+  									<p class="price"><h5>{{product.price}} ฿</h5> </p>
+                    {{product.amount}} left <br>
+                    <a class="button is-link is-outlined">Add to cart</a>
 							</li>
 						</ul>
 						<hr>
-
-						<!-- <div class="pagination pagination-small pagination-centered">
-							<ul>
-								<li><a href="/">Prev</a></li>
-								<li class="active"><a href="/">1</a></li>
-								<li><a href="/">2</a></li>
-								<li><a href="/">3</a></li>
-								<li><a href="/">4</a></li>
-								<li><a href="/">Next</a></li>
-							</ul>
-						</div> -->
-					</div>
-					<div class="col-2">
-
-						<h2>Best</h2>
-
 					</div>
 				</div>
-        <div  class="span12"style="text-align: left">
-          ชื่อ <input type="text" value="" v-model="n">
-          ราคา <input type="text" value="" v-model="p">
-          จำนวน <input type="text" value="" v-model="a"><br>
-          ลิ้งก์ <input type="text" value="" v-model="l">
-          <button @click="addProduct">add</button>
+        <div class="">
+          <h1>Add product</h1> <br>
         </div>
-				<div class="span12">
 
-					<div class="span12">
-						<p class="logo"><img src="./assets/themes/images/logo.png" class="site_logo" alt=""></p>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. the  Lorem Ipsum has been the industry's standard dummy text ever since the you.</p>
-						<br/>
-						<span class="social_icons">
-							<a class="facebook" href="/">Facebook</a>
-							<a class="twitter" href="/">Twitter</a>
-							<a class="skype" href="/">Skype</a>
-							<a class="vimeo" href="/">Vimeo</a>
-						</span>
-					</div>
-				</div>
+        <div class="row" style="margin-bottom: 30px">
 
-			<section class="span12">
-				<span>Copyright 2013 bootstrappage template  All right reserved.</span>
-			</section>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Name" v-model="n" >
+          </div>
+          <div class="col">
+            <input type="number" class="form-control" min="1" placeholder="Price" v-model="p" >
+          </div>
+          <div class="col">
+            <input type="number" class="form-control" min="1" placeholder="Amount" v-model="a" >
+          </div>
+          <div class="col">
+            <input type="text" class="form-control" placeholder="Image link" v-model="l" >
+          </div>
+          <button class="btn btn-success" @click="addProduct">Add</button>
+        </div>
+
 		</div>
 		</div>
 
@@ -83,15 +61,6 @@
       <li v-for="todo in todosFire" :key="todo['.key']">
         <input type="text" :value="todo.text" @input="updateTodoFire(todo['.key'], $event.target.value)">
         <button @click="removeTodoFire(todo['.key'])">X</button>
-      </li>
-    </ul> -->
-    <!-- local -->
-    <!-- <input type="text" v-model="text">
-    <button @click="addTodo"> Add </button><br>
-    <ul>
-      <li v-for="todo in todos" :key="todo.key">
-        <input type="text" :value="todo.text" @input="updateTodo(todo.key, $event.target.value)">
-        <button @click="removeTodo(todo.key)">X</button>
       </li>
     </ul> -->
 </template>
@@ -124,19 +93,77 @@ export default {
       addProductPassword: '',
       passwordForAdd: '123456',
       onAdd: false,
-      p: '',
-      n: '',
-      l: '',
-      a: ''
+      p: null,
+      n: null,
+      l: null,
+      a: null
     }
   },
   methods: {
+
     addProduct () {
-      productRef.push({
-        productName: this.n,
-        price: this.p,
-        imgLink: this.l
-      })
+      // swal(
+      //   'Good job!',
+      //   'You clicked the button!',
+      //   'success'
+      //   )
+      console.log(this.p)
+      console.log(this.n)
+      console.log(this.a)
+      console.log(this.l)
+      if (this.n!==null||this.p!==null||this.a!==null||this.l!==null) {
+        if (this.n!==null) {
+          if (this.p!==null&&this.p>=0) {
+            if (this.a!==null&&this.a>0) {
+              if (this.l!==null) {
+                productRef.push({
+                  productName: this.n,
+                  price: this.p,
+                  imgLink: this.l,
+                  amount: this.a
+                })
+                this.n = null
+                this.l = null
+                this.p = null
+                this.a = null
+              }else {
+                swal(
+                  'Warning!',
+                  'Please fill in image\'s link of product!',
+                  'warning'
+                  )
+              }
+            }else {
+              swal(
+                'Warning!',
+                'Please fill in the correct amount of product!',
+                'warning'
+                )
+            }
+          }else {
+            swal(
+              'Warning!',
+              'Please fill in the correct price of product!',
+              'warning'
+              )
+          }
+        }else {
+          swal(
+            'Warning!',
+            'Please fill in name of product!',
+            'warning'
+            )
+        }
+      }else {
+        swal(
+          'Warning!',
+          'Please fill up the information!',
+          'warning'
+          )
+      }
+
+
+
     },
     addTodoFire () {
       todosRef.push({
@@ -186,26 +213,48 @@ export default {
 </script>
 
 <style>
-#app {
+  #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: -5px;
-}
-ul {
+  }
+  ul {
     margin: 0 auto;
     text-align: center;
-}
+  }
 
-li {
+  li {
     display: inline-block;
     vertical-align: top;
-}
-.imgEdit {
+    margin-bottom: 10px
+  }
+  .imgEdit {
     border-radius: 50%;
     width: 300px;
     height: 300px;
-}
+  }
+  h1 {
+     font-size: 50px;
+     color: black;
+     text-align: center;
+     text-shadow: 0px 1px 0px #f2f2f2;
+     border: 1px solid transparent;
+   }
+  h2 {
+     font-size: 35px;
+     color: black;
+     text-align: center;
+     margin: 0 0 35px 0;
+     text-shadow: 0px 1px 0px #f2f2f2;
+   }
+ h5 {
+      font-size: 20px;
+      color: black;
+      text-align: center;
+      font-weight: bold;
+      text-shadow: 0px 1px 0px #f2f2f2;
+    }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+/* h1, h2 {
   font-weight: normal;
 }
 ul {
@@ -27,5 +27,5 @@ li {
 }
 a {
   color: #42b983;
-}
+} */
 </style>
